bccc380986f27f838dc2f18368b4ce9a59df5f87
Michael Hohenstein <michael@hohenste.in>
2024-06-25 15:03:04 +0200

    added configuration shift register in LC and also testing with 4 LCs

---
+++ overlay/LC/SerialLC.vhd
@@ -0,0 +1,49 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use IEEE.math_real.all;  -- for the power function
+
+entity SerialLC is
+	generic(
+		NUM_INPUTS : positive := 2
+	);
+	port(
+		-- LUT interface
+		Inputs : in std_logic_vector(NUM_INPUTS-1 downto 0);
+		Previous : in std_logic;
+		FF_Clock : in std_logic;
+		Output : out std_logic;
+		
+		-- configuration shift register
+		serin, serclk : in std_logic;
+		serout : out std_logic
+	);
+end entity;
+
+architecture arch of SerialLC is
+	constant config_size : positive := NUM_INPUTS**2 + 2;  -- Number of slots in the LUT + the Prev-Multiplexer + D-FF Enable
+	signal lc_config : std_logic_vector(config_size-1 downto 0) := (others => '0');
+	signal lut_in : std_logic;
+begin
+	lut_in <= Previous when lc_config(0) = '1' else Inputs(0);
+
+	LC : entity work.LogicCell
+		generic map(NUM_INPUTS => 2)
+		port map(
+			Inputs => (lut_in, Inputs(1)),
+			Config => lc_config(config_size-1 downto 2),
+			FF_enable => lc_config(1),
+			Clock => FF_Clock,
+			Output => Output
+		);
+		
+	process(serclk) is
+	begin
+		if rising_edge(serclk) then
+			lc_config(config_size-1 downto 1) <= lc_config(config_size-2 downto 0);
+			lc_config(0) <= serin;
+		end if;
+	end process;
+	serout <= lc_config(config_size-1);
+	
+end architecture;
--- overlay/Overlay.vhd
+++ overlay/Overlay.vhd
@@ -1,39 +1,57 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+library LC;
 
 entity Overlay is
 	generic(
-		Num_Luts : positive := 2
+		Group_Size : positive := 4;
+		Num_Groups : positive := 1  -- currently unused
 	);
 	port(
-		clock : in std_logic;
-		clock_baud : in std_logic;
 		Button : in std_logic_vector(3 downto 0);
 		Switch : in std_logic_vector(7 downto 0);
-		LED_Red : out std_logic_vector(7 downto 0);
-		LED_Green : out std_logic_vector(7 downto 0)
+		LED_Red, LED_Green : out std_logic_vector(7 downto 0);
+		
+		serin, serclk : in std_logic;
+		serout : out std_logic
 	);
 end entity;
 
 architecture bhv of Overlay is
-	signal lc1_out : std_logic;
-begin
+	-- conversion signals for the LUT interface
+	type t_inputs is array(Group_Size downto 1) of std_logic_vector(1 downto 0);
+	signal temp_inputs : t_inputs;
+	signal temp_clock : std_logic;
+	signal temp_output : std_logic_vector(Group_Size downto 0) := (others => '0');  -- one more because of the "previous" chain, output 0 does not exists but needs to be assigned
 
-	LC1 : entity work.LogicCell
-		generic map(NUM_INPUTS => 2)
-		port map(
-			Inputs => Button(1 downto 0),
-			Config => Switch(3 downto 0),
-			FF_enable => Switch(4),
-			Clock => clock,
-			Output => lc1_out
-		);
+	signal serial_connect : std_logic_vector(Group_Size+1 downto 1);  -- connect the config shift registers with each other
+begin	
 
-	-- The following line is just used to test the lut and the clock constraints
-	LED_Red <= (0 => not(clock),
-					1 => not(clock_baud),
-					2 => lc1_out,
+	LC_Group	: for i in 1 to Group_Size generate
+		LCG1 : LC.SerialLC
+			generic map(NUM_INPUTS => 2)
+			port map(
+				Inputs => temp_inputs(i),
+				Previous => temp_output(i-1),  -- previous from following LUT
+				FF_Clock => temp_clock,
+				Output => temp_output(i),
+				serin => serial_connect(i),
+				serclk => serclk,
+				serout => serial_connect(i+1)
+			);
+		end generate;
+
+	-- assign pins for testing purpose
+	temp_inputs <= (1 => Switch(1 downto 0), 2=> Switch(3 downto 2), 3 => Switch(5 downto 4),  4 => Switch(7 downto 6));
+	temp_clock <= Button(0);
+	LED_Red <= (0 => temp_output(1),
+					1 => temp_output(2), 
+					2 => temp_output(3),
+					3 => temp_output(4),
 					others => '0');
+--	LED_Red <= (Group_Size-1 downto 0 => temp_output(Group_Size downto 1), others => '0');
+	serial_connect(1) <= serin;
+	serout <= serial_connect(Group_Size+1);
 
 end architecture;
