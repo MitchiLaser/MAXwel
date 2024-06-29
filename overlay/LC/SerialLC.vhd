509161c3357c6855c8da87c520557aa1adfdf584
Michael Hohenstein <michael@hohenste.in>
2024-06-30 01:00:18 +0200

    overlay: remove mixing of one-indexed and zero-indexed vectors

--- overlay/LC/SerialLC.vhd
+++ overlay/LC/SerialLC.vhd
@@ -1,7 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use IEEE.math_real.all;  -- for the power function
 
 entity SerialLC is
 	generic(
--- overlay/Overlay.vhd
+++ overlay/Overlay.vhd
@@ -25,15 +25,15 @@ architecture bhv of Overlay is
 
 	-- define the signals for the serial shift register chain (LCs)
 	-- array (Groups) of std_logic_vector(Group)
-	type t_ser_bridge is array(Num_Groups-1 downto 0) of std_logic_vector(Group_Size+1 downto 1);  -- one more because the output of the last one needs to be assigned to the next one in the chain
+	type t_ser_bridge is array(Num_Groups-1 downto 0) of std_logic_vector(Group_Size downto 0);  -- one more because the output of the last one needs to be assigned to the next one in the chain
 	signal ser_bridge_lc : t_ser_bridge;
 
 	-- inputs: 2d array (Group of LCs) of std_logic_vectors(2 inputs)
-	type t_inputs is array(Num_Groups-1 downto 0, Group_Size downto 1) of std_logic_vector(1 downto 0);
+	type t_inputs is array(Num_Groups-1 downto 0, Group_Size-1 downto 0) of std_logic_vector(1 downto 0);
 	signal lc_inputs : t_inputs;
 	
 	-- clocks for the FFs in the LCs
-	type t_ff_clock is array(Num_Groups-1 downto 0) of std_logic_vector(Group_Size downto 1);
+	type t_ff_clock is array(Num_Groups-1 downto 0) of std_logic_vector(Group_Size-1 downto 0);
 	signal ff_clock : t_ff_clock;
 	
 	-- outputs of the LCs: 1D array (Groups) of std_logic_vectors (whole Group).
@@ -45,46 +45,53 @@ begin
 	-- generate the array of Logic Cells (LC)
 	LC_ALL : for i in 0 to Num_Groups-1 generate  -- generate all groups
 	begin
-		LC_Group	: for j in 1 to Group_Size generate  -- generate the LCs inside a Group
+		LC_Group	: for j in 0 to Group_Size-1 generate  -- generate the LCs inside a Group
 		begin
-			Cell : LC.SerialLC
-				generic map(NUM_INPUTS => 2)
-				port map(
-					Inputs => lc_inputs(i,j),
-					Previous => lc_output(i)(j-1),  -- previous from following LUT
-					FF_Clock => ff_clock(i)(j),
-					Output => lc_output(i)(j),
-					serin => ser_bridge_lc(i)(j),
-					serclk => serclk,
-					serout => ser_bridge_lc(i)(j+1)
-				);
+		
+			-- create first cell in group without 'previous'
+			First_Cell : if j = 0 generate 
+				First : LC.SerialLC
+					generic map(NUM_INPUTS => 2)
+					port map(
+						Inputs => lc_inputs(i,j),
+						Previous => '0',  -- previous not existing
+						FF_Clock => ff_clock(i)(j),
+						Output => lc_output(i)(j),
+						serin => ser_bridge_lc(i)(j),
+						serclk => serclk,
+						serout => ser_bridge_lc(i)(j+1)
+					);
+			end generate;
+				
+			-- create all other cells in group with 'previous'
+			Following_Cell : if j /= 0 generate
+				Following : LC.SerialLC
+					generic map(NUM_INPUTS => 2)
+					port map(
+						Inputs => lc_inputs(i,j),
+						Previous => lc_output(i)(j-1),  -- previous from following LUT
+						FF_Clock => ff_clock(i)(j),
+						Output => lc_output(i)(j),
+						serin => ser_bridge_lc(i)(j),
+						serclk => serclk,
+						serout => ser_bridge_lc(i)(j+1)
+					);
 			end generate;
 			
 			-- connect the serout from the last of a group to the serin from the first of a group
 			ser_fill : if i /= 0 generate
-				ser_bridge_lc(i)(1) <= ser_bridge_lc(i-1)(Group_Size+1);
+				ser_bridge_lc(i)(0) <= ser_bridge_lc(i-1)(Group_Size);
 			end generate;
 			
-			-- initialise the 'previous' value of the first object in a group
-			lc_output(i)(0) <= '0';
+		end generate;
 	end generate;
 	
 	-- initialise the serial chain
-	ser_bridge_lc(0)(1) <= serin;
+	ser_bridge_lc(0)(0) <= serin;
+	-- connect serial output -- TODO: update to the lastest link in the chain
+	serout <= ser_bridge_lc(Num_Groups-1)(Group_Size);
 	
 	
 	-- TODO: Now define the switching network and also the i/o blocks (if needed?)
-	
-
-	-- assign pins for testing purpose
---	temp_inputs <= (1 => Switch(1 downto 0), 2=> Switch(3 downto 2), 3 => Switch(5 downto 4),  4 => Switch(7 downto 6));
---	temp_clock <= Button(0);
---	LED_Red <= (0 => temp_output(1),
---					1 => temp_output(2), 
---					2 => temp_output(3),
---					3 => temp_output(4),
---					others => '0');
---	serial_connect(1) <= serin;
---	serout <= serial_connect(Group_Size+1);
 
 end architecture;
