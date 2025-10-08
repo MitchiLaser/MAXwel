bb7bea7d8336ad453ba7f4fad78668be50472418
Michael Hohenstein <michael@hohenste.in>
2025-10-08 13:50:52 +0200

    Update names to be equal in docs and template

--- template/vhdl/template.vhd
+++ template/vhdl/template.vhd
@@ -4,11 +4,11 @@ use ieee.numeric_std.all;
 
 entity template is
 	port(
-		Buttons : in std_logic_vector(3 downto 0);  -- Button 4 to Button 1
-	Switches : in std_logic_vector(7 downto 0); -- Switch 8 to switch 1
+		Buttons : in std_logic_vector(3 downto 0);  -- Button 3 to Button 0
+	Switches : in std_logic_vector(7 downto 0); -- Switch 7 to switch 0
 		Clock50, Clock14 : in std_logic;  -- both clocks
-		LED_A : out std_logic_vector(7 downto 0);  -- From LED 8 to LED 1
-		LED_B : out std_logic_vector(7 downto 0);  -- From LED 8 to LED 1
+		LED_A : out std_logic_vector(7 downto 0);  -- From LED 7 to LED 0
+		LED_B : out std_logic_vector(7 downto 0);  -- From LED 7 to LED 0
 		Segment1, Segment2, Segment3, Segment4 : out	std_logic_vector(7 downto 0)  -- From DP (7) to A (0) as defined in the docs 
 		-- The pinheader needs to be defined by the user
 	);
